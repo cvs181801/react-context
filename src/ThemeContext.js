234f0b4dde8899caa3ab818df9f52e147d98c8e7
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-07 13:27:12 -0800

    moving themecontext into it's own component

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,3 +1,13 @@
-import React from 'react'
-const ThemeContext = React.createContext()
-export default ThemeContext
+import React, {Component} from 'react'
+const {Provider, Consumer} = React.createContext()
+
+class ThemeContextprovider extends Component {
+    render() {
+        return(
+            <ThemeContextProvider value={"Kiley"}>
+                {this.props.children}
+            </ThemeContextProvider>
+        )
+    }
+}
+export {ThemeContextProvider, Consumer as ThemeContextConsumer}
