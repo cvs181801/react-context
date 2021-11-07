e80c88c72a9b02ebb21c4e9077e0d2754eb7384d
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-07 10:32:57 -0800

    add context - switched the App component from a functional to a class component to enable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import ThemeContext from './ThemeContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    console.log(this.context)
+      return (
+          <div>
+              <Header />
+              <main>
+                  <p className="main">No new notifications, {this.context}! 🎉</p>
+                
+              </main>
+          </div>
+      )
+  }
 }
 
-export default App;
+App.contextType = ThemeContext
+
+export default App
